9e0eb577beea04ce513757c7cd69a6fdb995273e
Day <itsdayagain@protonmail.com>
2026-02-13 18:25:45 +0100

    Preview

--- js/main.js
+++ js/main.js
@@ -1,93 +1,4 @@
 //
-// COOKIE CONSENT
-//
-
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-
-// Consent when user accepts all cookies (we only use analytics atm)
-const ACCEPT_CONSENT = {
-  'ad_storage': 'denied',
-  'ad_user_data': 'denied',
-  'ad_personalization': 'denied',
-  'analytics_storage': 'granted',
-};
-
-// Consent when user rejects cookies, or has not yet made a choice
-const REJECT_CONSENT = {
-  'ad_storage': 'denied',
-  'ad_user_data': 'denied',
-  'ad_personalization': 'denied',
-  'analytics_storage': 'denied',
-};
-
-const DEFAULT_CONSENT = REJECT_CONSENT
-
-const DEBUG = false;
-function debug_log() {
-  DEBUG && console.log(arguments);
-}
-
-/** Returns user settings from storage, or null if not set. */
-function loadConsentSettings(storage = localStorage) {
-  consent = JSON.parse(storage.getItem('consent'));
-  debug_log('Loaded consent...', consent);
-  return consent;
-}
-
-/** Saves user settings to storage. */
-function saveConsentSettings(consent, storage = localStorage) {
-  debug_log('Saving consent...', consent);
-  storage.setItem('consent', JSON.stringify(consent));
-}
-
-/** Sets consent with gtag. Op should be either 'default' or 'update'. */
-function setConsent(consent, op) {
-  debug_log('Setting consent with gtag...', consent, op);
-  gtag('consent', op, consent);
-  document.dispatchEvent(new CustomEvent("consentChanged", { detail: consent }));
-}
-
-// Set defaults based on user preference, before loading Google Analytics
-gtag('consent', 'default', DEFAULT_CONSENT);
-
-// Did user already make a selection in the past?
-settings = loadConsentSettings();
-if (settings) {
-  // Yes: Set it accordingly
-  debug_log('Cookie settings found:', settings);
-  setConsent(settings, 'update');
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-  const dialog = document.getElementById('cookie-consent-dialog');
-  if (!dialog) {
-    console.error('Cookie consent dialog not found.');
-    return false;
-  }
-  const accept_button = document.getElementById('accept-cookies-btn');
-  const reject_button = document.getElementById('reject-cookies-btn');
-
-  accept_button.addEventListener('click', function () {
-    debug_log('Handling accept...')
-    saveConsentSettings(ACCEPT_CONSENT);
-    setConsent(ACCEPT_CONSENT, 'update');
-  });
-  reject_button.addEventListener('click', function () {
-    debug_log('Handling reject...')
-    saveConsentSettings(REJECT_CONSENT);
-    setConsent(REJECT_CONSENT, 'update');
-  });
-
-  // Has user not yet made their selection? Show the banner if so.
-  // Note that this can be disabled by setting data-disable_cookie_banner="false" on <body>
-  if (!loadConsentSettings() && !document.body.dataset.disable_cookie_banner) {
-    debug_log('User has not set cookie settings yet. Showing dialog...');
-    dialog.show();
-  }
-});
-
-//
 // GEO-BLOCK
 //
 
@@ -229,4 +140,39 @@ document.addEventListener('DOMContentLoaded', async function () {
   if (target_el) {
     loadMessages(getMessages, target_el, 0);
   }
+});
+
+//
+// NTF FLOOR PRICE
+//
+
+function setNftFloorPrice(target_el) {
+  const me_url = 'https://api-mainnet.magiceden.dev/v2/collections/hegends/stats';
+  const sol_url = 'https://api.diadata.org/v1/assetQuotation/Solana/0x0000000000000000000000000000000000000000';
+  const cors_proxy = 'https://cors.io/?url='
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json'
+    }
+  };
+  fetch(cors_proxy + encodeURIComponent(sol_url), options)
+    .then(res => res.json())
+    .then(data => {
+      sol_d = JSON.parse(data.body);
+      fetch(cors_proxy + encodeURIComponent(me_url), options)
+        .then(res => res.json())
+        .then(data => {
+          me_d = JSON.parse(data.body);
+          const usd_price = (sol_d.Price * me_d.floorPrice / 1000000000).toFixed(2);
+          target_el.innerHTML = '$' + usd_price;
+        })
+        .catch(err => console.error(err));
+    })
+    .catch(err => console.error(err));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  const target = document.getElementById('hegends-floor-price');
+  setNftFloorPrice(target);
 });
